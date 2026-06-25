7d74793e71629d65c3203c8c74219336aaf822db
anajuliacarvalho08-glitch <ana.julia.carvalho08@escola.pr.gov.br>
2026-06-25 20:02:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let pontos = 0;
+let arvores = 0;
 
-function ganharPontos(){
-    pontos += 10;
-    document.getElementById("pontos").textContent = pontos;
+function resposta(tipo){
+
+    const msg = document.getElementById("mensagem");
+
+    if(tipo === "certo"){
+        pontos += 20;
+        msg.innerHTML = "✅ Boa escolha! +20 pontos";
+    }else{
+        pontos -= 10;
+        msg.innerHTML = "❌ Isso prejudica o meio ambiente!";
+    }
+
+    document.getElementById("pontos").innerHTML = pontos;
+}
+
+function plantar(){
+    arvores++;
+    pontos += 5;
+
+    document.getElementById("arvores").innerHTML = arvores;
+    document.getElementById("pontos").innerHTML = pontos;
+
+    if(arvores === 5){
+        alert("🏆 Parabéns! Você ganhou a medalha Guardião da Natureza!");
+    }
 }
